d3867ec1660ec4b1c2c2e35e1632d7e711946c88
William Caldwell <wocaldwell@gmail.com>
2017-07-26 16:22:33 -0500

    updated msome cv items

--- app/controllers/CurriculumVitaeCtrl.js
+++ app/controllers/CurriculumVitaeCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("CurriculumVitaeCtrl", function($scope, $window, $location){
+app.controller("CurriculumVitaeCtrl", function($scope, $window, $location, JsonFactory){
 
     // make this link appear "active"
     $("#portfolio-link").removeClass("active");
@@ -12,5 +12,14 @@ app.controller("CurriculumVitaeCtrl", function($scope, $window, $location){
 
     $(document).ready(function(){
         $('.parallax').parallax();
+    });
+
+    JsonFactory.getJsonData("assets/jsons/cv.json")
+    .then(function(returnedData){
+        $scope.professionalExperience = returnedData.professionalExperience;
+        $scope.education = returnedData.education;
+        $scope.researchInterests = returnedData.researchInterests;
+        $scope.service = returnedData.service;
+        console.log($scope.service);
     });
 });
